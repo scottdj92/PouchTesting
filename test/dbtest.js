96d9fba063ff57b14ac4c335042f9a751265eca6
Scott Jones <sjones4@sapient.com>
2016-05-13 12:23:04 -0400

    Inital scaffold created. Need more code regarding server and tests

---
+++ test/dbtest.js
@@ -0,0 +1,7 @@
+var chai = require('chai');
+var assert = chai.assert();
+
+describe('Testing Connection', function() {
+	it('should return an object');
+	it('should return the database name');
+});
